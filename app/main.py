0db0f978a3a97f7b3f2868e029e2e054139a1c9a
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-09-20 16:53:34 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def main():
@@ -13,6 +14,13 @@ def main():
         elif command[5:] in ["type", "echo", "exit"]:
             print(f"{command[5:]} is a shell builtin")
         else:
+            os_path = os.get_exec_path()
+            path_line = 0
+            for path_line in os_path:
+                # print(os.access(path_line, os.X_OK))
+                if path_line.endswith(command[5:]) and os.access(path_line, os.X_OK):
+                    print(f"{command[5:]} is {path_line}")
+                    break
             print(f"{command.strip('type ')}: not found")
 
 
