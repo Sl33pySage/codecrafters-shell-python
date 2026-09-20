e6f56606f7fea402ccddd1e34e77e3d827659962
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-09-20 11:05:37 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def main():
         elif command[5:] in ["type", "echo", "exit"]:
             print(f"{command[5:]} is a shell builtin")
         else:
-            print(f"{command.strip('type')}: not found")
+            print(f"{command.strip('type ')}: not found")
 
 
 if __name__ == "__main__":
