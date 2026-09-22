bacdddd8a7e88c217c784fff0b10845b1e627e61
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-09-22 14:57:45 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -28,7 +28,6 @@ def main():
 
         else:
             parts = command.split()
-            print(parts)
 
             if parts and shutil.which(parts[0]):
                 subprocess.run(parts)
