75b6e176eaf35b57720b6249c5ceac3ade65447f
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-09-21 23:20:00 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,33 +1,46 @@
-import sys
-import os
-import shutil
-import subprocess
+import subprocess, shutil, os, sys
+
+
+def builtin_commands(c):
+    return c in {"echo", "exit", "type"}
+
+
+def execute_commands(c):
+    for d in os.get_exec_path():
+        if os.access(fullpath := os.path.join(d, c), os.X_OK):
+            return fullpath
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        sys.stdout.flush()
+        command = input().strip()
 
-        if command == "exit":
-            break
-        elif command.startswith("echo"):
-            print(command[5:])
-        elif command.startswith("type"):
-            cmd = command[5:]
+        if not command:
+            continue
 
-            if cmd in ["type", "echo", "exit"]:
-                print(f"{command[5:]} is a shell builtin")
+        parts = command.split()
+        cmd = parts[0]
+        args = parts[1:] if len(parts) > 1 else ""
 
-            elif path := shutil.which(cmd):
-                print(f"{cmd} is {path}")
+        if builtin_commands(cmd):
+            if cmd == "exit":
+                break
+        elif cmd == "echo":
+            print(f"{' '.join(args)}")
+        elif cmd == "type":
+            if builtin_commands(args[0]):
+                print(f"{args[0]} is a shell builtin")
+            elif fullpath := execute_commands(args[0]):
+                print(f"{args[0]} is {fullpath}")
             else:
-                if shutil.which(cmd):
-                    subprocess.run([f"{cmd}"], check=False, capture_output=True)
+                print(f"{args[0]}: not found")
 
-                    print(f"{cmd}: not found")
+        elif execute_commands(cmd):
+            subprocess.run(parts)
         else:
-            print(f"{command}: not found")
+            print(f"{cmd}: command not found")
 
 
 if __name__ == "__main__":
